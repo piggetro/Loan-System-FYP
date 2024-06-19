18e10166164a5364044a1e54634bd894cea605a7
Franc Bobis <103973085+francb-228@users.noreply.github.com>
2024-06-20 00:45:08 +0800

    feat(Staff Types): done

--- src/app/(protected)/profile/page.tsx
+++ src/app/(protected)/profile/page.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import TopHeaderComponent from "@/app/_components/TopHeader";
+import React from 'react';
+import { api } from "@/trpc/server";
+import StaffType from "./_components/StaffType";
 
-const page = () => {
-  return <div>profile</div>;
+const page = async () => {
+  const data = await api.staffTypes.getAllStaffTypes();
+
+  return (
+    <div>
+      <TopHeaderComponent
+        pathName="School Admin / Staff Types"
+        pageName="Staff Types"
+      />
+      <StaffType data={data} />
+    </div>
+  );
 };
 
 export default page;
